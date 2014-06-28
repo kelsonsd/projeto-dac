4104227eff53d13e4ac53b3cf327e80ce4ebe89f
kelsonsd <kelsonsd@premium>
2014-06-28 00:51:56 -0300

    Correções e atualizações;

---
+++ src/main/java/br/edu/ifpb/dac/app/Main.java
@@ -0,0 +1,14 @@
+package br.edu.ifpb.dac.app;
+
+import br.edu.ifpb.dac.frames.principal.MenuPrincipal;
+
+/**
+ *
+ * @author kelsonsd
+ */
+
+public class Main {
+    public static void main(String args[]) {        
+        new MenuPrincipal().setVisible(true);        
+    }
+}
